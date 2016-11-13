d70a85529456446d16ae38a399af1e27b8df704e
Olzhas Aitkaliyev <olzhas.aitkaliyev@gmail.com>
2016-11-13 23:27:58 +0300

    Added plot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
 import java.awt.image.BufferedImage;
 
+
 /**
- * Created by laptop on 12.11.2016.
+ * Created by olzhas on 12.11.2016.
  */
 public class Main {
     public static void main(String [] args) {
@@ -9,11 +10,11 @@ public class Main {
         BufferedImage map = ImageConvert.loadImage(path);
         Terrain cityMap = ImageConvert.convertTo2D(map);
         cityMap.calculateRadiation();
-        //System.out.println("radiation at start " + cityMap.getTerrain()[cityMap.getStart().y()][cityMap.getStart().x()].getRadiation());
+        System.out.println("radiation at start " + cityMap.getTerrain()[cityMap.getStart().y()][cityMap.getStart().x()].getRadiation());
         OptimalPath escape = new OptimalPath(cityMap);
         escape.calculateOptimalPath(1400.0);
         System.out.println(escape.showOptimalPath());
 
-
+        Plot plot = new Plot(cityMap);
     }
 }
--- src/main/java/Plot.java
+++ src/main/java/Plot.java
@@ -1,6 +1,59 @@
 /**
- * Created by laptop on 12.11.2016.
+ * Created by olzhas on 12.11.2016.
  */
 
-public class Plot {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class Plot extends JPanel{
+    private Terrain terrain;
+
+    public Plot(Terrain terrain) {
+        this.terrain = terrain;
+        JFrame frame = new JFrame("Radiation map");
+        frame.add(this);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(700, 700);
+        frame.setVisible(true);
+
+        try {
+            BufferedImage image = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+            Graphics2D graphics2D = image.createGraphics();
+            frame.paint(graphics2D);
+            ImageIO.write(image,"jpeg", new File("radiation.jpeg"));
+        } catch (Exception exception) {
+            //code
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        this.setBackground(Color.WHITE);
+        Place[][] places = terrain.getTerrain();
+        for (int i = 0; i < places.length; i++) {
+            for (int j = 0; j < places[i].length; j++) {
+                double r = places[i][j].getRadiation();
+                float rad = (float) r;
+                g.setColor(Color.getHSBColor(rad / 3500000, 1, 1));
+
+                if (places[i][j].isPath()) {
+                    g.setColor(Color.WHITE);
+                } else if (places[i][j].isStart()) {
+                    g.setColor(Color.BLUE);
+                } else if (places[i][j].isFinish()) {
+                    g.setColor(Color.GREEN);
+                } else if (places[i][j].getType() == 1) {
+                    g.setColor(Color.BLACK);
+                } else if (places[i][j].getType() == 2) {
+                    g.setColor(Color.ORANGE);
+                }
+                g.drawLine(i, j, i, j);
+            }
+        }
+    }
+
+
 }
